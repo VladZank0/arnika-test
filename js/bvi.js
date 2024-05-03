3e15973ff12a221fa82669b7803ed1e037641fa5
VladZank0 <vlad.zanko.2019@mail.ru>
2024-05-03 13:46:15 +0300

    .

--- js/bvi.js
+++
@@ -1,9 +0,0 @@
- 
- new isvek.Bvi({
-    target: '.bvi-open', // Класс инициализации
-    fontSize: 24, // Размер шрифта
-    theme: 'black', // Цветовая тема сайта
-    // panelFixed: true,
-    panelHide: true
-});
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJidmkuanMiXSwic291cmNlc0NvbnRlbnQiOlsiIFxyXG4gbmV3IGlzdmVrLkJ2aSh7XHJcbiAgICB0YXJnZXQ6ICcuYnZpLW9wZW4nLCAvLyDQmtC70LDRgdGBINC40L3QuNGG0LjQsNC70LjQt9Cw0YbQuNC4XHJcbiAgICBmb250U2l6ZTogMjQsIC8vINCg0LDQt9C80LXRgCDRiNGA0LjRhNGC0LBcclxuICAgIHRoZW1lOiAnYmxhY2snLCAvLyDQptCy0LXRgtC+0LLQsNGPINGC0LXQvNCwINGB0LDQudGC0LBcclxuICAgIC8vIHBhbmVsRml4ZWQ6IHRydWUsXHJcbiAgICBwYW5lbEhpZGU6IHRydWVcclxufSk7XHJcbiJdLCJmaWxlIjoiYnZpLmpzIn0=
