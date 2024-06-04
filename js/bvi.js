3cefa53e6ab8936e83d370d55047d2c0fb62c61a
VladZank0 <vlad.zanko.2019@mail.ru>
2024-06-04 17:04:34 +0300

    .

--- js/bvi.js
+++
@@ -1,8 +0,0 @@
- 
- new isvek.Bvi({
-    target: '.bvi-open', // Класс инициализации
-    fontSize: 24, // Размер шрифта
-    theme: 'black', // Цветовая тема сайта
-    // panelFixed: true,
-    panelHide: true
-});
